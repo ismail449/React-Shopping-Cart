a6585e7bf7a3afd603278ede39877a336bbe9605
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-04-02 15:42:57 +0200

    adding the header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header Component/Header';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='layout' >
+      <Header />
+      <main>main</main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
