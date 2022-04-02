52bc158b8b898bded879596caa975fdf6c5771cf
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-04-02 17:45:40 +0200

    adding the data file

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    title: 'Camera',
+    imageUrl: '/images/camera.jpg',
+    price: 400,
+    size: ['M'],
+  },
+  {
+    id: 2,
+    title: 'High Heels',
+    imageUrl: '/images/heels.jpg',
+    price: 100,
+    size: ['S', 'M', 'L'],
+  },
+  {
+    id: 3,
+    title: 'Apple Watch',
+    imageUrl: '/images/watch.jpg',
+    price: 600,
+    size: ['M'],
+  },
+  {
+    id: 4,
+    title: 'Sunglasses',
+    imageUrl: '/images/sunglasses.jpg',
+    price: 50,
+    size: ['S', 'M', 'L'],
+  },
+];
+
+export default data;
